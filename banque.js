b36205b99a317e17f2cae07c09dd1b9e6674ef6c
Creepynax <116552630+Creepynax@users.noreply.github.com>
2022-11-25 12:11:23 +0100

    2nd TO DO

--- banque.js
+++ banque.js
@@ -8,4 +8,13 @@ form.addEventListener('submit', function(event){
     event.preventDefault();
     const formulaireCompte = document.querySelector("#compte")
     const formulaireSomme = document.querySelector("#somme")
+
+    // Conditions qui vérifient que les champs du formulaire sont correctes
+
+    if(formulaireCompte.value.length < 16){
+        alert("Veuillez rentrer 16 chiffres");
+    }
+    if(formulaireSomme.value < 0){
+        alert("Veuillez rentrer une somme supérieur à 0");
+    }
 });
