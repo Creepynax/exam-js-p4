71fe66c4abc00d722082ad6fe8899f3b0af7e3bf
Creepynax <116552630+Creepynax@users.noreply.github.com>
2022-11-25 12:40:17 +0100

    4th TO DO

--- banque.js
+++ banque.js
@@ -9,18 +9,23 @@ form.addEventListener('submit', function(event){
     const formulaireCompte = document.querySelector("#compte")
     const formulaireSomme = document.querySelector("#somme")
 
+    // Condition qui lorsque tous les champs sont correctes, renvoie une alert précisant la somme déposée 
+    if(formulaireCompte.value.length == 16 && formulaireSomme.value > 0){
+        alert("La somme de " + formulaireSomme.value + " a bien été déposée")
+    }
     // Conditions qui vérifient que les champs du formulaire sont correctes
-
-    if(formulaireCompte.value.length != 16){
-        alert("Veuillez rentrer 16 chiffres");
-        formulaireCompte.style.backgroundColor = "white";
-    }
-
-    // Change le couleur du fond du compte lorsqu'il y a bien 16 chiffres dedans
     else{
-        formulaireCompte.style.backgroundColor = "#6eff33";
-    }
-    if(formulaireSomme.value < 0){
-        alert("Veuillez rentrer une somme supérieur à 0");
+        if(formulaireCompte.value.length != 16){
+            alert("Veuillez rentrer 16 chiffres");
+            formulaireCompte.style.backgroundColor = "white";
+        }
+        // Change le couleur du fond du compte lorsqu'il y a bien 16 chiffres dedans
+        else{
+            formulaireCompte.style.backgroundColor = "#6eff33";
+        }
+    
+        if(formulaireSomme.value < 0){
+            alert("Veuillez rentrer une somme supérieur à 0");
+        }
     }
 });
